ac367baeb285300fd3d76f772e47104bf8d131a5
ramraj666 <pmaurya196@gmail.com>
2019-01-11 02:00:15 +0530

    Added recipe-list template

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipes-list',
@@ -7,6 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipesListComponent implements OnInit {
 
+  recipes: Recipe[] = [
+    new Recipe("Test","A test Recipe","https://cdn.apartmenttherapy.info/image/fetch/f_auto,q_auto,w_600,h_750,c_fit,fl_strip_profile/https://s3.amazonaws.com/pixtruder/original_images/17c6ec7292ce74c4f38c71ee4816925c46ffd4bf")
+  ]
   constructor() { }
 
   ngOnInit() {
